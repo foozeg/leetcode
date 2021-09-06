9227aa7c7e409e2024eb8e52591499d5c23718b6
foozeg <foozeg1@gmail.com>
2021-09-06 20:39:15 +0300

    Update 013_roman_to_integer.py

--- 013_roman_to_integer.py
+++ 013_roman_to_integer.py
@@ -18,13 +18,12 @@ class Solution:
             if i != len(s)-1:
                 if number is 1 and (s[i+1] == 'V' or s[i+1] == 'X'):
                     number *= -1
-                if number is 10 and (s[i+1] == 'L' or s[i+1] == 'C'):
+                elif number is 10 and (s[i+1] == 'L' or s[i+1] == 'C'):
                     number *= -1
-                if number is 100 and (s[i+1] == 'D' or s[i+1] == 'M'):
+                elif number is 100 and (s[i+1] == 'D' or s[i+1] == 'M'):
                     number *= -1
             int_array.append(number)
-        #int_array = [switch[i] for i in s]
-        
+            
         return sum(int_array)
 #Result
 #Runtime: 77 ms, faster than 10.22% of Python3 online submissions for Roman to Integer.
