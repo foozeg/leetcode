6f8e50f1eb38daeced3e7e0d9418a13edc36c32d
foozeg <foozeg1@gmail.com>
2021-09-06 10:24:26 +0300

    Update 002_add_two_numbers.py

--- 002_add_two_numbers.py
+++ 002_add_two_numbers.py
@@ -39,3 +39,42 @@ class Solution:
 #Results
 #Runtime: 72 ms, faster than 57.11% of Python3 online submissions for Add Two Numbers.
 #Memory Usage: 14.4 MB, less than 12.92% of Python3 online submissions for Add Two Numbers.
+
+#Let's try solution without recursion
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution2:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        first = True
+        pop = 0
+        while True:
+            val3 = l1.val + l2.val + pop
+            pop = int(val3/10)
+            val3 = val3 % 10
+            #print(l1.val, l2.val)
+
+            if first:
+                l3 = ListNode(val3)
+                first = False
+                last = l3
+            else:
+                new = ListNode(val3)
+                last.next = new
+                last = new
+                
+                #l3 = ListNode(val3, next=l3)
+                
+
+            if l1.next is None and l2.next is None and pop == 0:
+                break
+
+            l1 = ListNode(0) if (l1.next is None) else l1.next
+            l2 = ListNode(0) if (l2.next is None) else l2.next
+        
+        return l3
+#Results
+#Runtime: 72 ms, faster than 57.58% of Python3 online submissions for Add Two Numbers.
+#Memory Usage: 14.2 MB, less than 90.56% of Python3 online submissions for Add Two Numbers.
