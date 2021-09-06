7b69b50dc863f241cf6ebc8a950ec5a0239ace35
foozeg <foozeg1@gmail.com>
2021-09-06 20:34:43 +0300

    Create 13_roman_to_integer.py

---
+++ 13_roman_to_integer.py
@@ -0,0 +1,31 @@
+#13. Roman to Integer
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        switch = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+        
+        int_array = []
+        for i in range(len(s)):
+            number = switch[s[i]]
+            if i != len(s)-1:
+                if number is 1 and (s[i+1] == 'V' or s[i+1] == 'X'):
+                    number *= -1
+                if number is 10 and (s[i+1] == 'L' or s[i+1] == 'C'):
+                    number *= -1
+                if number is 100 and (s[i+1] == 'D' or s[i+1] == 'M'):
+                    number *= -1
+            int_array.append(number)
+        #int_array = [switch[i] for i in s]
+        
+        return sum(int_array)
+#Result
+#Runtime: 77 ms, faster than 10.22% of Python3 online submissions for Roman to Integer.
+#Memory Usage: 14.4 MB, less than 26.93% of Python3 online submissions for Roman to Integer.
