372eabaf1956b006f468867f9c04f71437354e23
foozeg <foozeg1@gmail.com>
2021-09-06 20:48:24 +0300

    Update 013_roman_to_integer.py

--- 013_roman_to_integer.py
+++ 013_roman_to_integer.py
@@ -1,6 +1,6 @@
 #13. Roman to Integer
 
-class Solution:
+class Solution1:
     def romanToInt(self, s: str) -> int:
         switch = {
             'I' : 1,
@@ -28,3 +28,31 @@ class Solution:
 #Result
 #Runtime: 77 ms, faster than 10.22% of Python3 online submissions for Roman to Integer.
 #Memory Usage: 14.4 MB, less than 26.93% of Python3 online submissions for Roman to Integer.
+
+#After picking into solutions on leetcode
+class Solution2:
+    def romanToInt(self, s: str) -> int:
+        switch = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+        
+        int_array = []
+        for i in range(len(s)):
+            number = switch[s[i]]
+            if i != len(s)-1:
+                next_number = switch[s[i+1]]
+                #Roman numbers are always in descending order unless one is negative
+                if (number < next_number):
+                    number *= -1
+            int_array.append(number)
+        
+        return sum(int_array)
+#Result
+#Runtime: 52 ms, faster than 48.56% of Python3 online submissions for Roman to Integer.
+#Memory Usage: 14.3 MB, less than 58.67% of Python3 online submissions for Roman to Integer.
