9d307366ef415de442d03f0a9d4054c22bdec278
foozeg <foozeg1@gmail.com>
2021-09-12 21:21:29 +0300

    Update 021_merge_two_sorted_lists.py

--- 021_merge_two_sorted_lists.py
+++ 021_merge_two_sorted_lists.py
@@ -6,6 +6,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
         output = None
@@ -13,38 +14,27 @@ class Solution:
         
         while l1 and l2:
             if l1.val <= l2.val:
-                if output == None:
-                    output, last = l1, l1
-                else:
-                    last.next = l1
-                    last = last.next
-                l1 = l1.next
+                output, last, l1 = self.process(output, last, l1)
             else:
-                if output == None:
-                    output, last = l2, l2
-                else:
-                    last.next = l2
-                    last = last.next
-                l2 = l2.next
+                output, last, l2 = self.process(output, last, l2)
         
         while l1:
-            if output == None:
-                output, last = l1, l1
-            else:
-                last.next = l1
-                last = last.next
-            l1 = l1.next
+            output, last, l1 = self.process(output, last, l1)
             
         while l2:
-            if output == None:
-                output, last = l2, l2
-            else:
-                last.next = l2
-                last = last.next
-            l2 = l2.next
+            output, last, l2 = self.process(output, last, l2)
         
         return output
-            
+        
+    def process(self, output, last, link_list):
+        if output == None:
+            output, last = link_list, link_list
+        else:
+            last.next = link_list
+            last = last.next
+        
+        return output, last, link_list.next
+  
 #Results:
-#Runtime: 55 ms, faster than 13.78% of Python3 online submissions for Merge Two Sorted Lists.
-#Memory Usage: 14 MB, less than 99.54% of Python3 online submissions for Merge Two Sorted Lists.
+#Runtime: 32 ms, faster than 92.46% of Python3 online submissions for Merge Two Sorted Lists.
+#Memory Usage: 14.4 MB, less than 30.86% of Python3 online submissions for Merge Two Sorted Lists.
